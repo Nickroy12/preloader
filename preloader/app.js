f418f01db26a5dbabe7f300db2ba87566d1ea7c9
Nick roy <83406410+Nickroy12@users.noreply.github.com>
2023-10-25 22:32:42 -0700

    Add files via upload

---
+++ preloader/app.js
@@ -0,0 +1,8 @@
+// When the window finishes loading
+window.addEventListener('load', function() {
+    // Hide the preloader
+    document.querySelector('.preloader').style.display = 'none';
+
+    // Show the content
+    document.querySelector('.content').style.display = 'block';
+});
